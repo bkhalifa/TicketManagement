a458bac7e9240f21ecdd315415a5a09b95289b11
BILELBEN <benkhalifa.bilel@hotmail.fr>
2023-03-05 18:04:32 +0100

    first

---
+++ GloboTicket.TicketManagement.Application/Features/Categories/Commands/CreateCateogry/CreateCategoryCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
+
+namespace GloboTicket.TicketManagement.Application.Features.Categories.Commands.CreateCateogry
+{
+    public class CreateCategoryCommand: IRequest<CreateCategoryCommandResponse>
+    {
+        public string Name { get; set; }
+    }
+}
